28ed01d89dce23832ef59856d4c9b8b1a0446e6c
Eric Niaina <ericniaina@msn.com>
2015-06-14 18:10:13 +0400

    Basic Moving Snake

--- public_html/js/basic_follow.js
+++ public_html/js/basic_follow.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {preload: 
 function preload() {
 
     game.load.image('background', 'assets/debug-grid-1920x1920.png');
-    game.load.image('player', 'assets/arrow.png');
+    game.load.image('arrow', 'assets/arrow.png');
 
 }
 
@@ -18,41 +18,16 @@ function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-    player.anchor.setTo(0.5, 0.5);
-
-    game.physics.enable(player);
-
     cursors = game.input.keyboard.createCursorKeys();
 
-    game.camera.follow(player);
-
+    player = new Snake(game, cursors);
+    player.create();
 }
 
 function update() {
-    player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
-    player.body.angularVelocity = 0;
-
-    if (cursors.left.isDown)
-    {
-        player.body.angularVelocity = -200;
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.angularVelocity = 200;
-    }
-
-//    if (cursors.up.isDown)
-//    {
-        game.physics.arcade.velocityFromAngle(player.angle, 300, player.body.velocity);
-//    }
-
+    player.update();
 }
 
 function render() {
-
-    game.debug.cameraInfo(game.camera, 32, 32);
-    game.debug.spriteCoords(player, 32, 500);
-
+    player.debug();
 }
---
+++ public_html/js/snake.js
@@ -0,0 +1,70 @@
+function Snake(game, cursors) {
+    var snakeHead; //head of snake sprite
+    var snakeSection = new Array(); //array of sprites that make the snake body sections
+    var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
+    var numSnakeSections = 30; //number of snake body sections
+    var snakeSpacer = 6; //parameter that sets the spacing between sections
+
+    var game = game;
+    var cursors = cursors;
+    this.create = function() {
+        snakeHead = game.add.sprite(400, 300, 'arrow');
+        snakeHead.anchor.setTo(0.5, 0.5);
+
+        game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
+
+        //  Init snakeSection array
+        for (var i = 1; i <= numSnakeSections - 1; i++)
+        {
+            snakeSection[i] = game.add.sprite(400, 300, 'arrow');
+            snakeSection[i].anchor.setTo(0.5, 0.5);
+        }
+
+        //  Init snakePath array
+        for (var i = 0; i <= numSnakeSections * snakeSpacer; i++)
+        {
+            snakePath[i] = new Phaser.Point(400, 300);
+        }
+
+        game.camera.follow(snakeHead);
+
+    }
+
+    this.update = function() {
+
+        snakeHead.body.velocity.setTo(0, 0);
+        snakeHead.body.angularVelocity = 0;
+
+        snakeHead.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(snakeHead.angle, 300));
+
+        // Everytime the snake head moves, insert the new location at the start of the array, 
+        // and knock the last position off the end
+
+        var part = snakePath.pop();
+
+        part.setTo(snakeHead.x, snakeHead.y);
+
+        snakePath.unshift(part);
+
+        for (var i = 1; i <= numSnakeSections - 1; i++)
+        {
+            snakeSection[i].x = (snakePath[i * snakeSpacer]).x;
+            snakeSection[i].y = (snakePath[i * snakeSpacer]).y;
+        }
+
+        if (cursors.left.isDown)
+        {
+            snakeHead.body.angularVelocity = -300;
+        }
+        else if (cursors.right.isDown)
+        {
+            snakeHead.body.angularVelocity = 300;
+        }
+
+    }
+
+    this.debug = function() {
+        game.debug.cameraInfo(game.camera, 32, 32);
+        game.debug.spriteCoords(snakeHead, 32, 500);
+    }
+}
