ce67e9176b65f405345cfa21da4c828c2384d80e
Eric Niaina <ericniaina@msn.com>
2015-06-14 15:49:38 +0400

    Project

---
+++ public_html/js/basic_follow.js
@@ -0,0 +1,61 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.image('background','assets/debug-grid-1920x1920.png');
+    game.load.image('player','assets/arrow.png');
+
+}
+
+var player;
+var cursors;
+
+function create() {
+
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+
+    game.world.setBounds(0, 0, 1920, 1920);
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+
+    game.physics.p2.enable(player);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.camera.follow(player);
+
+}
+
+function update() {
+
+    player.body.setZeroVelocity();
+    player.body.setZeroRotation();
+
+    if (cursors.up.isDown)
+    {
+        player.body.moveUp(300)
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.moveDown(300);
+    }
+
+    if (cursors.left.isDown)
+    {
+    	player.body.rotateLeft(75);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.rotateRight(75);
+    }
+
+}
+
+function render() {
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 32, 500);
+
+}
